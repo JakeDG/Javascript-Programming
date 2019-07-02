c2b829ebbc826a55e39504a2c42756fd5fa3f76f
JakeDG <32947306+JakeDG@users.noreply.github.com>
2019-07-01 20:09:10 -0400

    Remade project

--- ExpressApp/ExpressApp/routes/index.js
+++ ExpressApp/ExpressApp/routes/index.js
@@ -2,8 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-var getData = function ()
-{
+var getData = function () {
     var data = {
         'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
         'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
