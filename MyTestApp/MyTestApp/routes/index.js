3d7df271381fa1639c7eeaa046be0e8149e25930
JakeDG <32947306+JakeDG@users.noreply.github.com>
2019-07-02 01:43:44 -0400

    Created MyTestApp

---
+++ MyTestApp/MyTestApp/routes/index.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+var getData = function () {
+    var data = {
+        'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
+        'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
+        'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg'
+    }
+    return data;
+}
+
+var getRandomNumber = function () {
+    var min = 1
+    var max = 10
+    var num = (Math.random() * (+max - +min)) + +min; 
+
+    document.getElementById("output1").value = num.toFixed(2);
+}
+
+var print = function () {
+    document.getElementById("output1").value = "Hello, world!");
+}
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'JavavScript Test', "data": getData()});
+});
+
+module.exports = router;
